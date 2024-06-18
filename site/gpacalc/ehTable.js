cfb03442f3c55bc976722ebcae18ff0e57557083
Ehan A <ehan@ehanaha.me>
2024-06-18 11:39:19 -0400

    reinvent the wheel

---
+++ site/gpacalc/ehTable.js
@@ -0,0 +1,9 @@
+var ehTable = {
+    insert: function (table, index) {
+        var row = document.createElement("div");
+        row.classList.add("row");
+        table.insertBefore(
+            row,
+        )
+    }
+}
