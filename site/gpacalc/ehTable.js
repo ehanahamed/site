206331f549c2a3f08f3e91b73f786be2c4de6029
Ehan A <ehan@ehanaha.me>
2024-07-05 15:21:36 -0400

    tables?

--- site/gpacalc/ehTable.js
+++ site/gpacalc/ehTable.js
@@ -1,9 +1,32 @@
 var ehTable = {
-    insert: function (table, index) {
+    insertRow: function (element, index) {
         var row = document.createElement("div");
         row.classList.add("row");
-        table.insertBefore(
-            row,
-        )
+        if (index == undefined) {
+            element.appendChild(
+                row
+            )
+        } else {
+            element.insertBefore(
+                row,
+                element.children[index]
+            )
+        }
+    },
+    appendRow: function (element) {
+        ehTable.insertRow(element);
+    },
+    insertCol: function (element, index) {
+        var col = document.createElement("div");
+        col.classList.add("col");
+        if (index) {
+            element.insertBefore(
+                col,
+                element.children[index]
+            )
+        }
+    },
+    appendCol: function (element) {
+        ehTable.insertCol(element);
     }
 }
