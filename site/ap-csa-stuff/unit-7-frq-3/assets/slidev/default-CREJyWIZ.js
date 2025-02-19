a760ffdb427f3ebdfe81f15c941fd220a5d66b23
Ehan A <ehan@ehanaha.me>
2025-02-19 18:47:43 -0500

    use correct base arg for build command

--- site/ap-csa-stuff/unit-7-frq-3/assets/slidev/default-CREJyWIZ.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as t,j as s}from"../modules/vue-9isPCTa4.js";import{_ as r}from"../index-DXojn5CD.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
